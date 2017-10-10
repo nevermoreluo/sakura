0caac1d3545ab7785a78e46c1730d0ac0510605e
nevermore <nevermoreluo@gmail.com>
2017-10-10 12:17:45 +0800

    setup sakura

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='sakura',
+    packages=['sakura'],
+    version='0.1',
+    description='sakura',
+    author='None',
+    author_email='None',
+    maintainer='sakura',
+    maintainer_email='sakura@nonono.rocks',
+    url='https://github.com/nevermoreluo/sakura',
+    download_url='https://github.com/nevermoreluo/sakura/archive/master.zip',
+)
