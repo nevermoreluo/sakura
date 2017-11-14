ccc6d888379a55064133ebaf02f375a17b561e46
nevermore <nevermoreluo@gmail.com>
2017-11-14 14:52:52 +0800

    update setup version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='sakura',
     packages=['sakura'],
-    version='0.1.8',
+    version='0.1.9',
     description='sakura',
     author='None',
     author_email='None',
@@ -12,6 +12,7 @@ setup(
     url='https://github.com/nevermoreluo/sakura',
     download_url='https://github.com/nevermoreluo/sakura/archive/master.zip',
     install_requires=[
+        'wheel>=0.30.0',
         'pycrypto>=2.6.1',
         'python-dateutil>=2.6.1',
         'pytz==2017.2',
